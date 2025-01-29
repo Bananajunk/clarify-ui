04fbbd04eaad647cd02335519601839f17bac4fd
Liam Reid <liam.reid@faire.com>
2025-01-29 16:06:18 -0500

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import pluginPrettier from "eslint-config-prettier";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["src/**/*.{ts,tsx}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  { settings: { react: { version: "detect" } } },
+  pluginPrettier,
+];
