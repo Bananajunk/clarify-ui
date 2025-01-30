ae3a4ac0538763e2c8b0d32534b3ddaa05573dc6
Liam Reid <liam.reid@faire.com>
2025-01-30 09:13:42 -0500

    Setup initial tailwind config for storybook

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,7 +1,11 @@
 import type { ButtonProps } from "./Button.types";
 
 const Button: React.FC<ButtonProps> = ({ children }: ButtonProps) => {
-  return <button>{children}</button>;
+  return (
+    <button className="bg-blue-500 text-white p-2 rounded-md">
+      {children}
+    </button>
+  );
 };
 
 export default Button;
