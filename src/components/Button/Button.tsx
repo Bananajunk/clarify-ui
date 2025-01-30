954a40b105595d4a4c36e999e13eddb454ee9335
Liam Reid <liam.reid@faire.com>
2025-01-30 09:51:23 -0500

    Configured font families for storybook

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,7 +2,7 @@ import type { ButtonProps } from "./Button.types";
 
 const Button: React.FC<ButtonProps> = ({ children }: ButtonProps) => {
   return (
-    <button className="bg-blue-500 text-white p-2 rounded-md">
+    <button className="bg-blue-500 text-white p-2 rounded-md font-sans">
       {children}
     </button>
   );
